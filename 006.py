22b48976942546b25cd22f2fdd092a392c3de3ba
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2015-06-04 18:12:08 +0200

    add solution of #6

--- 006.py
+++ 006.py
@@ -15,4 +15,8 @@ Find the difference between the sum of the squares of the first one
 hundred natural numbers and the square of the sum.
 """
 
+def solve(n):
+    return sum(range(1, n+1))**2 - sum(x**2 for x in range(1, n+1))
 
+if __name__=='__main__':
+    print solve(100)
