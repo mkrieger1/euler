3f57ed5bf9826f7e687733cf8678ed7994be5592
Michael Krieger <mkrieger1@users.noreply.github.com>
2021-04-27 22:34:55 +0200

    Solve Problem 10 in Nim

---
+++ euler010.nim
@@ -0,0 +1,24 @@
+import math
+
+iterator sievePrimes(upperBound: Natural): Natural =
+  ## Sieve of Erathostenes
+  var notPrime = newSeq[bool](upperBound + 1)
+  let maxFactorToCheck = sqrt(upperBound.float).Natural
+
+  for candidate in 2..maxFactorToCheck:
+    if notPrime[candidate]:
+      continue
+    yield candidate
+    for multiple in countup(candidate, upperBound, candidate):
+      notPrime[multiple] = true
+
+  for candidate in maxFactorToCheck+1..upperBound:
+    if notPrime[candidate]:
+      continue
+    yield candidate
+
+func sumOfPrimesBelow(n: Natural): Natural =
+  for prime in sievePrimes(n):
+    result += prime
+
+echo sumOfPrimesBelow 2000000
