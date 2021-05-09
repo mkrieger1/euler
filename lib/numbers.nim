533035e5a39ec3d3cac6467b3e1ffb50abeffcb3
mkrieger1 <mkrieger1@users.noreply.github.com>
2021-05-09 11:51:05 +0200

    Add documentation with runnable example for primeDecomposition

--- lib/numbers.nim
+++ lib/numbers.nim
@@ -1,4 +1,13 @@
 iterator primeDecomposition*(n: Positive): Positive =
+  ## Generate all prime factors of `n`, in ascending order.
+  ##
+  ## Each factor can be generated multiple times.
+  runnableExamples:
+    var primes: seq[int]
+    for p in primeDecomposition(126):
+      primes.add p
+    assert primes == [2, 3, 3, 7]
+
   var rest = n
   var factor = 2
   while rest > 1:
